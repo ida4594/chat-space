836ba220af9fdb635b7bd27cb388d4d1849f1e40
ida4594 <kaha4594@yahoo.co.jp>
2019-11-17 19:11:33 +0900

    修正

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -29,6 +29,7 @@ $(function() {
     let html = `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
     $(`#${userId}`).append(html);
   }
+  document.addEventListener("turbolinks:load", function(){
   $("#user-search-field").on("keyup", function() {
     let input = $("#user-search-field").val();
     $.ajax({
@@ -54,6 +55,7 @@ $(function() {
         alert("通信エラーです。ユーザーが表示できません。");
       });
   });
+})
   $(document).on("click", ".chat-group-user__btn--add", function() {
     console.log
     const userName = $(this).attr("data-user-name");

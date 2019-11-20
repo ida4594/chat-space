152135d00d11762eecaff9d2fae125a3997a2508
ida4594 <kaha4594@yahoo.co.jp>
2019-11-20 21:09:39 +0900

    14

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,4 +1,4 @@
-class Uploader < CarrierWave::Uploader::Base
+class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
